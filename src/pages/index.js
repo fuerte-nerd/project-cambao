83341858dfd60c8f1ebea49cca1f6ac4cbf0d858
David Andrews <dandrewsuk82@gmail.com>
2020-04-29 00:33:32 +0100

    Autogit: 29-04-2020 00:33

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import { Box, Typography, Button } from "@material-ui/core"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -7,6 +8,24 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <Box
+      bgcolor="primary.light"
+      minWidth="100vw"
+      minHeight="100vh"
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Box>
+        <Typography></Typography>
+      </Box>
+      <Box>
+        <Typography></Typography>
+        <Typography></Typography>
+        <Button></Button>
+      </Box>
+    </Box>
   </Layout>
 )
 
