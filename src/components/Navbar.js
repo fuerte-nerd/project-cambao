db48a3f8a943af8ffe4ebf8126c748dbb7e146b0
David Andrews <dandrewsuk82@gmail.com>
2020-04-28 23:13:03 +0100

    Autogit: 28-04-2020 23:13

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,31 @@
 import React from "react"
-import { AppBar, Toolbar, IconButton, Typography } from "@material-ui/core"
+import { graphql, useStaticQuery } from "gatsby"
+import { Box, AppBar, Toolbar, IconButton, Typography } from "@material-ui/core"
 import { Menu } from "@material-ui/icons"
 import Img from "gatsby-image"
 
 const Navbar = () => {
+  const data = useStaticQuery(graphql`
+    {
+      file(name: { eq: "logo" }, sourceInstanceName: { eq: "images" }) {
+        childImageSharp {
+          fixed(height: 70, width: 70) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+        sourceInstanceName
+      }
+    }
+  `)
   return (
     <AppBar>
       <Toolbar>
         <IconButton>
           <Menu />
         </IconButton>
+        <Box my={1}>
+          <Img fixed={data.file.childImageSharp.fixed} />
+        </Box>
         <Typography variant="h5" variantMapping={{ h5: "h1" }}>
           FDR
         </Typography>
