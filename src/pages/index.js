9f5734b5b4060f8bb642fac7bf50aa276c56b0a8
David Andrews <dandrewsuk82@gmail.com>
2020-04-29 12:58:49 +0100

    Autogit: 29-04-2020 12:58

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, useStaticQuery } from "gatsby"
 import {
   Paper,
   Box,
@@ -8,37 +8,55 @@ import {
   Button,
   Container,
 } from "@material-ui/core"
+import BackgroundImage from "gatsby-background-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <Box
-      bgcolor="secondary.main"
-      minWidth="100vw"
-      minHeight="100vh"
-      display="flex"
-      flexDirection="column"
-      alignItems="center"
-      justifyContent="center"
-    >
-      <Grid container>
-        <Grid item xs={12} md={6}></Grid>
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    {
+      file(name: { eq: "test" }) {
+        childImageSharp {
+          fluid(maxWidth: 600) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <Box
+        bgcolor="secondary.main"
+        minWidth="100vw"
+        minHeight="100vh"
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Grid container>
+          <Grid item xs={12} md={6}>
+            <BackgroundImage
+              fluid={data.file.childImageSharp.fluid}
+              style={{ height: "100vh", width: "100%" }}
+            ></BackgroundImage>
+          </Grid>
 
-        <Grid item xs={12} md={6}>
-          <Container>
-            <Paper>
-              <Box p={3}>
-                <Typography>Hello</Typography>
-              </Box>
-            </Paper>
-          </Container>
+          <Grid item xs={12} md={6}>
+            <Container>
+              <Paper>
+                <Box p={3}>
+                  <Typography>Hello</Typography>
+                </Box>
+              </Paper>
+            </Container>
+          </Grid>
         </Grid>
-      </Grid>
-    </Box>
-  </Layout>
-)
-
+      </Box>
+    </Layout>
+  )
+}
 export default IndexPage
