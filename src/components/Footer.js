4e1907d6aa71e713621a5050b036fb5a65fc3fa6
David Andrews <dandrewsuk82@gmail.com>
2020-04-30 14:14:03 +0100

    Autogit: 30-04-2020 14:14

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,12 +3,16 @@ import { Box, Typography } from "@material-ui/core"
 
 const Footer = () => {
   return (
-    <Box>
+    <Box m={2} align="center">
+      <Typography display="block" variant="caption">
+        Registered Charity in the Canary Islands since April 2013 -
+        G1/S1/19399-13/F
+      </Typography>
       <Typography variant="caption">
         All content &copy; 2020
-        {Date().getCurrentYear() === 2020
+        {new Date().getFullYear() === 2020
           ? ` `
-          : ` - ${Date().getCurrentYear()}`}{" "}
+          : ` - ${new Date().getFullYear()}`}{" "}
         Fuerteventura Dog Rescue
       </Typography>
     </Box>
