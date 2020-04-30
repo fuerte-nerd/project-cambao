6061bbcbc6084ea0b026eafb9c1c91ec77223268
David Andrews <dandrewsuk82@gmail.com>
2020-04-30 15:48:24 +0100

    Autogit: 30-04-2020 15:48

--- src/pages/index.js
+++ src/pages/index.js
@@ -66,10 +66,10 @@ const IndexPage = () => {
           <CardActionArea>
             <CardContent>
               <Grid container spacing={2}>
-                <Grid item xs={12} sm={5} md={4} lg={6}>
+                <Grid item xs={12} sm={5} md={4} lg={3}>
                   <Img fluid={data.dog1.childImageSharp.fluid} />
                 </Grid>
-                <Grid item xs={12} sm={7} md={8} lg={6}>
+                <Grid item xs={12} sm={7} md={8} lg={9}>
                   <Typography variant="h3">Post title</Typography>
                   <Typography variant="overline">27 April 2020</Typography>
                   <Box mb={2}>
