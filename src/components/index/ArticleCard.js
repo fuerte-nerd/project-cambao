eba2ed7ba76701762111e351e64aaddb3b453e1e
David Andrews <dandrewsuk82@gmail.com>
2020-05-02 18:39:07 +0100

    Autogit: 02-05-2020 18:39

---
+++ src/components/index/ArticleCard.js
@@ -0,0 +1,62 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  Grid,
+  Typography,
+  Button,
+  Divider,
+} from "@material-ui/core"
+import Img from "gatsby-image"
+
+const ArticleCard = () => {
+  const data = useStaticQuery(graphql`
+    {
+      dog1: file(name: { eq: "test" }) {
+        childImageSharp {
+          fluid(maxWidth: 500, maxHeight: 400, quality: 15) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <Card>
+      <CardActionArea>
+        <CardContent>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={5} md={4} lg={3}>
+              <Img fluid={data.dog1.childImageSharp.fluid} />
+            </Grid>
+            <Grid item xs={12} sm={7} md={8} lg={9}>
+              <Typography variant="h3">Post title</Typography>
+              <Typography variant="overline">27 April 2020</Typography>
+              <Box mb={2}>
+                <Divider />
+              </Box>
+
+              <Typography align="justify" paragraph>
+                Lorem possimus non perspiciatis quibusdam iste. Numquam
+                veritatis consequatur velit ea ad quia? Deleniti autem dolorum
+                consequatur labore natus. Obcaecati assumenda debitis quas
+                accusantium temporibus animi! Facilis molestias ab
+                necessitatibus similique itaque Exercitationem assumenda nemo.
+              </Typography>
+              <Box align="end">
+                <Button variant="outlined" color="secondary">
+                  Read more
+                </Button>
+              </Box>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  )
+}
+
+export default ArticleCard
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,88 +1,19 @@
 import React from "react"
-import { graphql, useStaticQuery } from "gatsby"
-import Img from "gatsby-image"
-import {
-  useMediaQuery,
-  Card,
-  CardMedia,
-  CardHeader,
-  CardContent,
-  CardActionArea,
-  CardActions,
-  Button,
-  Divider,
-  Hidden,
-  List,
-  ListSubheader,
-  ListItem,
-  ListItemText,
-  Box,
-  Grid,
-  Typography,
-  Container,
-  useTheme,
-} from "@material-ui/core"
+import { useMediaQuery, useTheme } from "@material-ui/core"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Heading from "../components/index/Heading"
+import ArticleCard from "../components/index/ArticleCard"
 
 const IndexPage = () => {
   const theme = useTheme()
   const isNotMobile = useMediaQuery(theme.breakpoints.up("sm"))
-  const data = useStaticQuery(graphql`
-    {
-      dog1: file(name: { eq: "test" }) {
-        childImageSharp {
-          fluid(maxWidth: 500, maxHeight: 400, quality: 15) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-      logo: file(name: { eq: "logo" }) {
-        childImageSharp {
-          fluid(maxWidth: 205, maxHeight: 205) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-    }
-  `)
   return (
     <Layout>
       <SEO title="Home" />
       <Heading />
-      <Card>
-        <CardActionArea>
-          <CardContent>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={5} md={4} lg={3}>
-                <Img fluid={data.dog1.childImageSharp.fluid} />
-              </Grid>
-              <Grid item xs={12} sm={7} md={8} lg={9}>
-                <Typography variant="h3">Post title</Typography>
-                <Typography variant="overline">27 April 2020</Typography>
-                <Box mb={2}>
-                  <Divider />
-                </Box>
-
-                <Typography align="justify" paragraph>
-                  Lorem possimus non perspiciatis quibusdam iste. Numquam
-                  veritatis consequatur velit ea ad quia? Deleniti autem dolorum
-                  consequatur labore natus. Obcaecati assumenda debitis quas
-                  accusantium temporibus animi! Facilis molestias ab
-                  necessitatibus similique itaque Exercitationem assumenda nemo.
-                </Typography>
-                <Box align="end">
-                  <Button variant="outlined" color="secondary">
-                    Read more
-                  </Button>
-                </Box>
-              </Grid>
-            </Grid>
-          </CardContent>
-        </CardActionArea>
-      </Card>
+      <ArticleCard />
     </Layout>
   )
 }
