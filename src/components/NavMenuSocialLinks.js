cbdc9b3901535bf9310bcb51737d544c419e685e
David Andrews <dandrewsuk82@gmail.com>
2020-05-03 20:03:20 +0100

    Autogit: 03-05-2020 20:03

--- src/components/NavMenuSocialLinks.js
+++ src/components/NavMenuSocialLinks.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { makeStyles, Box } from "@material-ui/core"
 import { Facebook, Instagram } from "@material-ui/icons"
+import { FacebookMessenger } from "mdi-material-ui"
 import NavMenuSocialLinksItem from "./NavMenuSocialLinksItem"
 
 const useStyles = makeStyles(theme => ({
@@ -13,6 +14,9 @@ const NavMenuSocialLinks = () => {
   const classes = useStyles()
   return (
     <Box>
+      <NavMenuSocialLinksItem tooltip="Contact us on Messenger!">
+        <FacebookMessenger className={classes.socialButton} />
+      </NavMenuSocialLinksItem>
       <NavMenuSocialLinksItem tooltip="Visit us on Facebook!">
         <Facebook className={classes.socialButton} />
       </NavMenuSocialLinksItem>
