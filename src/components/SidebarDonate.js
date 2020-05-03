ccd835d51dcebce20bc03f149070c6b28333e843
David Andrews <dandrewsuk82@gmail.com>
2020-05-03 18:22:14 +0100

    Autogit: 03-05-2020 18:22

--- src/components/SidebarDonate.js
+++ src/components/SidebarDonate.js
@@ -1,11 +1,26 @@
 import React from "react"
-import { Box } from "@material-ui/core"
+import { Box, Typography, Button } from "@material-ui/core"
+import { EuroSymbol } from "@material-ui/icons"
 import SidebarSectionTitle from "./SidebarSectionTitle"
 
 const SidebarDonate = () => {
   return (
     <Box>
       <SidebarSectionTitle title="Donate" />
+      <Box pb={2}>
+        <Typography variant="caption">
+          We are a volunteer organization and appreciate anything you can
+          spare...
+        </Typography>
+        <Button
+          variant="contained"
+          fullWidth
+          color="secondary"
+          startIcon={<EuroSymbol />}
+        >
+          Make a donation
+        </Button>
+      </Box>
     </Box>
   )
 }
