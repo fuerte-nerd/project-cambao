cd89dd64abe977d3767aa2744e040d98df4be8b5
David Andrews <dandrewsuk82@gmail.com>
2020-05-03 21:49:24 +0100

    Autogit: 03-05-2020 21:49

--- src/components/SidebarQuickLinksSubmenu.js
+++ src/components/SidebarQuickLinksSubmenu.js
@@ -1,22 +1,32 @@
 import React from "react"
 import { Box, List, ListItem, ListItemText } from "@material-ui/core"
+import InternalLink from "./InternalLink"
 
 const SidebarQuickLinksSubmenu = () => {
   return (
     <Box p={1} bgcolor="#fafafa">
       <List dense disablePadding>
-        <ListItem button>
-          <ListItemText primary="Adopt" />
-        </ListItem>
-        <ListItem button>
-          <ListItemText primary="Foster" />
-        </ListItem>
-        <ListItem button>
-          <ListItemText primary="Donate" />
-        </ListItem>
-        <ListItem button>
-          <ListItemText primary="Volunteer" />
-        </ListItem>
+        <InternalLink to="/adopt">
+          <ListItem button>
+            <ListItemText primary="Adopt" />
+          </ListItem>
+        </InternalLink>
+
+        <InternalLink to="/foster">
+          <ListItem button>
+            <ListItemText primary="Foster" />
+          </ListItem>
+        </InternalLink>
+        <InternalLink to="/donate">
+          <ListItem button>
+            <ListItemText primary="Donate" />
+          </ListItem>
+        </InternalLink>
+        <InternalLink to="/volunteer">
+          <ListItem button>
+            <ListItemText primary="Volunteer" />
+          </ListItem>
+        </InternalLink>
       </List>
     </Box>
   )
