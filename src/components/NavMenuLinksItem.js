a445db7e47392c7f35ba686c234cc6d0421c2f64
David Andrews <dandrewsuk82@gmail.com>
2020-05-03 11:23:04 +0100

    Autogit: 03-05-2020 11:23

--- src/components/NavMenuLinksItem.js
+++ src/components/NavMenuLinksItem.js
@@ -5,20 +5,21 @@ import { ExpandLess, ExpandMore } from "@material-ui/icons"
 
 const NavMenuLinksItem = props => {
   return (
-    <ListItem
-      button
-      component={<AniLink fade to="the-dogs" />}
-      id={props.id}
-      divider={props.divider ? true : false}
-      onClick={props.clickEvent}
-    >
-      <ListItemText
-        primary={props.title}
-        secondary={props.subtitle}
-        primaryTypographyProps={{ style: { color: "#fafafa" } }}
-      />
-      {props.dropdown ? props.isOpen ? <ExpandLess /> : <ExpandMore /> : null}
-    </ListItem>
+    <AniLink fade to="the-dogs">
+      <ListItem
+        button
+        id={props.id}
+        divider={props.divider ? true : false}
+        onClick={props.clickEvent}
+      >
+        <ListItemText
+          primary={props.title}
+          secondary={props.subtitle}
+          primaryTypographyProps={{ style: { color: "#fafafa" } }}
+        />
+        {props.dropdown ? props.isOpen ? <ExpandLess /> : <ExpandMore /> : null}
+      </ListItem>
+    </AniLink>
   )
 }
 
