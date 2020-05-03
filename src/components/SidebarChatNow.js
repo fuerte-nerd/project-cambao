d365fb684f1984a5408850524e9d4f8b79144dd2
David Andrews <dandrewsuk82@gmail.com>
2020-05-03 21:08:33 +0100

    Autogit: 03-05-2020 21:08

--- src/components/SidebarChatNow.js
+++ src/components/SidebarChatNow.js
@@ -1,12 +1,18 @@
 import React from "react"
 import { Box, Typography, Button } from "@material-ui/core"
+import { FacebookMessenger } from "mdi-material-ui"
+import { Email } from "@material-ui/icons"
 import SidebarSectionTitle from "./SidebarSectionTitle"
 
 const SidebarChatNow = () => {
   return (
     <Box>
       <SidebarSectionTitle title="Send a message" />
-      <Typography variant="caption">If you need to get in touch...</Typography>
+      <Typography variant="caption">Contact us now via...</Typography>
+      <Box>
+        <Button startIcon={<FacebookMessenger />}>Messenger</Button>
+        <Button startIcon={<Email />}>Email</Button>
+      </Box>
     </Box>
   )
 }
