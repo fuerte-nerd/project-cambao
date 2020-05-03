c1910f4ccf91a9719b325e0cbb2d71f0ec240991
David Andrews <dandrewsuk82@gmail.com>
2020-05-03 10:07:20 +0100

    Autogit: 03-05-2020 10:07

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,27 +4,29 @@ import LocationMap from "./LocationMap"
 
 const Footer = () => {
   return (
-    <Box m={2} pt={2} width="100%" align="center" boxShadow={3}>
-      <Box mb={2}>
-        <Typography variant="h4" paragraph>
-          Where are we?
+    <Box mt={2} pt={2} bgcolor="primary.dark" align="center" boxShadow={3}>
+      <Box m={2} pt={2}>
+        <Box mb={2}>
+          <Typography variant="h4" paragraph>
+            Where are we?
+          </Typography>
+          <LocationMap />
+        </Box>
+        <Typography display="block" variant="caption">
+          Registered Charity in the Canary Islands since April 2013
+          (G1/S1/19399-13/F)
         </Typography>
-        <LocationMap />
+        <Typography display="block" variant="caption">
+          All content &copy; 2020
+          {new Date().getFullYear() === 2020
+            ? ` `
+            : ` - ${new Date().getFullYear()}`}{" "}
+          Fuerteventura Dog Rescue
+        </Typography>
+        <Typography display="block" variant="caption">
+          Site by dandroos
+        </Typography>
       </Box>
-      <Typography display="block" variant="caption">
-        Registered Charity in the Canary Islands since April 2013
-        (G1/S1/19399-13/F)
-      </Typography>
-      <Typography display="block" variant="caption">
-        All content &copy; 2020
-        {new Date().getFullYear() === 2020
-          ? ` `
-          : ` - ${new Date().getFullYear()}`}{" "}
-        Fuerteventura Dog Rescue
-      </Typography>
-      <Typography display="block" variant="caption">
-        Site by dandroos
-      </Typography>
     </Box>
   )
 }
