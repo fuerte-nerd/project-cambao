aafea0e2756ccdc04cd29d3e0257d5008bf23032
David Andrews <dandrewsuk82@gmail.com>
2020-05-03 12:08:52 +0100

    Autogit: 03-05-2020 12:08

--- src/components/InternalLink.js
+++ src/components/InternalLink.js
@@ -1,11 +1,15 @@
 import React from "react"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
+import colors from "../../colors"
 
 const InternalLink = props => {
   return (
     <AniLink
       to={props.to}
-      swipe
+      cover
+      direction="down"
+      duration={5}
+      bg={colors.fdr_blue}
       style={{ textDecoration: "none", color: "inherit" }}
     >
       {props.children}
