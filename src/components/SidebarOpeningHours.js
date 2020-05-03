b177888df2a360fdd17b86857bf797200bebd996
David Andrews <dandrewsuk82@gmail.com>
2020-05-03 15:20:19 +0100

    Autogit: 03-05-2020 15:20

--- src/components/SidebarOpeningHours.js
+++ src/components/SidebarOpeningHours.js
@@ -1,11 +1,38 @@
 import React from "react"
-import { Typography, Box } from "@material-ui/core"
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Paper,
+  Typography,
+  Box,
+} from "@material-ui/core"
 import SidebarSectionTitle from "./SidebarSectionTitle"
 
 const SidebarOpeningHours = () => {
   return (
     <Box>
       <SidebarSectionTitle title="Opening hours" />
+      <TableContainer>
+        <Table>
+          <TableBody>
+            <TableRow>
+              <TableCell>Thu</TableCell>
+              <TableCell>8:00 - 9:30</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Sat & Sun</TableCell>
+              <TableCell>9:00 - 10:30</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Holidays</TableCell>
+              <TableCell>9:00 - 10:30</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
       <Typography display="block">Thu 8-9:30</Typography>
       <Typography display="block">Sat 9-10:30</Typography>
       <Typography display="block">Sun 9-10:30</Typography>
