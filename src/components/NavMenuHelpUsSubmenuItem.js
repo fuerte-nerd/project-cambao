fefa9cbc7dd67b619d710c52bf465bd2c503f70a
David Andrews <dandrewsuk82@gmail.com>
2020-05-03 21:54:33 +0100

    Autogit: 03-05-2020 21:54

--- src/components/NavMenuHelpUsSubmenuItem.js
+++ src/components/NavMenuHelpUsSubmenuItem.js
@@ -1,15 +1,18 @@
 import React from "react"
 import { ListItem, ListItemText } from "@material-ui/core"
+import InternalLink from "./InternalLink"
 
 const NavMenuHelpUsSubmenuItem = props => {
   return (
-    <ListItem button id={props.id} divider>
-      <ListItemText
-        primary={props.title}
-        style={{ textAlign: "center" }}
-        primaryTypographyProps={{ style: { fontSize: ".75rem" } }}
-      />
-    </ListItem>
+    <InternalLink to={props.link}>
+      <ListItem button id={props.id} divider>
+        <ListItemText
+          primary={props.title}
+          style={{ textAlign: "center" }}
+          primaryTypographyProps={{ style: { fontSize: ".75rem" } }}
+        />
+      </ListItem>
+    </InternalLink>
   )
 }
 
