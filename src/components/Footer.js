c6a9e7627c30339af2b19f267f2420034deabff4
David Andrews <dandrewsuk82@gmail.com>
2020-05-03 15:40:37 +0100

    Autogit: 03-05-2020 15:40

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Box, Typography, Container, Hidden } from "@material-ui/core"
+import { Grid, Box, Typography, Container, Hidden } from "@material-ui/core"
 import LocationMap from "./LocationMap"
 
 const Footer = () => {
@@ -12,27 +12,29 @@ const Footer = () => {
       align="center"
       boxShadow={6}
     >
-      <Box p={2} pt={2}>
-        <Box pb={2}>
-          <Typography variant="h4" paragraph>
-            Where are we?
+      <Box p={2}>
+        <Grid container>
+          <Box pb={2}>
+            <Typography variant="h4" paragraph>
+              Where are we?
+            </Typography>
+            <LocationMap />
+          </Box>
+          <Typography display="block" variant="caption">
+            Registered Charity in the Canary Islands since April 2013
+            (G1/S1/19399-13/F)
           </Typography>
-          <LocationMap />
-        </Box>
-        <Typography display="block" variant="caption">
-          Registered Charity in the Canary Islands since April 2013
-          (G1/S1/19399-13/F)
-        </Typography>
-        <Typography display="block" variant="caption">
-          All content &copy; 2020
-          {new Date().getFullYear() === 2020
-            ? ` `
-            : ` - ${new Date().getFullYear()}`}{" "}
-          Fuerteventura Dog Rescue
-        </Typography>
-        <Typography display="block" variant="caption">
-          Site by dandroos
-        </Typography>
+          <Typography display="block" variant="caption">
+            All content &copy; 2020
+            {new Date().getFullYear() === 2020
+              ? ` `
+              : ` - ${new Date().getFullYear()}`}{" "}
+            Fuerteventura Dog Rescue
+          </Typography>
+          <Typography display="block" variant="caption">
+            Site by dandroos
+          </Typography>
+        </Grid>
       </Box>
     </Box>
   )
