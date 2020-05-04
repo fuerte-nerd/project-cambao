f0ebb93d81f313968fd62d5bdf75287fdc552d00
David Andrews <dandrewsuk82@gmail.com>
2020-05-04 14:02:44 +0100

    Autogit: 04-05-2020 14:02

--- src/pages/the-dogs.js
+++ src/pages/the-dogs.js
@@ -7,6 +7,9 @@ const TheDogs = () => {
     <>
       <SEO title="The Dogs" />
       <Typography variant="h2">The Dogs</Typography>
+      <Typography variant="subtitle1">
+        We have lots of dogs waiting for their forever home. Come and meet them!
+      </Typography>
     </>
   )
 }
