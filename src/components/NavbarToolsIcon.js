bacd841f6d5e143c62f2b7f247085d1c8f2bb630
David Andrews <dandrewsuk82@gmail.com>
2020-05-04 09:32:02 +0100

    Autogit: 04-05-2020 09:32

--- src/components/NavbarToolsIcon.js
+++ src/components/NavbarToolsIcon.js
@@ -2,15 +2,18 @@ import React from "react"
 import { withStyles, Tooltip, IconButton } from "@material-ui/core"
 
 const NavbarToolsIcon = props => {
-
   const test = withStyles({
     root: {
-      fontSize=".6rem"
-    }
+      fontSize: ".6rem",
+    },
   })
   return (
     <Tooltip title={props.tooltip}>
-      <IconButton color="inherit" classes={{root: {fontSize: ".6rem"}}  style={{ fontSize: ".7rem" }}>
+      <IconButton
+        color="inherit"
+        classes={{ root: { fontSize: ".6rem" } }}
+        style={{ fontSize: ".7rem" }}
+      >
         {props.children}
       </IconButton>
     </Tooltip>
