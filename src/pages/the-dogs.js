a4fc2c7bea40d8b58e5915c00c0cc7c72a156ac6
David Andrews <dandrewsuk82@gmail.com>
2020-05-04 14:07:53 +0100

    Autogit: 04-05-2020 14:07

--- src/pages/the-dogs.js
+++ src/pages/the-dogs.js
@@ -1,6 +1,18 @@
 import React from "react"
-import { Box, Typography } from "@material-ui/core"
+import {
+  Box,
+  Typography,
+  Card,
+  CardMedia,
+  CardHeader,
+  CardActions,
+  CardContent,
+  CardActionArea,
+  Button,
+} from "@material-ui/core"
 import SEO from "../components/seo"
+
+import tempImage from "../images/test.jpg"
 
 const TheDogs = () => {
   return (
@@ -9,7 +21,18 @@ const TheDogs = () => {
       <Typography variant="h2">The Dogs</Typography>
       <Typography variant="subtitle1">
         We have lots of dogs waiting for their forever home. Come and meet them!
+        They can't wait to meet you!
       </Typography>
+      <Box>
+        <Card>
+          <CardActionArea>
+            <CardMedia image={tempImage} />
+            <CardContent>
+              <CardHeader>Buddy</CardHeader>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Box>
     </>
   )
 }
