41d9f378ebc18e883414a800a2f9d79c737b86cc
David Andrews <dandrewsuk82@gmail.com>
2020-05-05 15:48:00 +0100

    Autogit: 05-05-2020 15:48

--- src/pages/dog.js
+++ src/pages/dog.js
@@ -27,10 +27,11 @@ const Dog = () => {
     }
   `)
   const smUp = useMediaQuery(theme.breakpoints.up("sm"))
+  const mdUp = useMediaQuery(theme.breakpoints.up("md"))
 
   return (
     <Box>
-      <Grid container spacing={smUp ? 1 : 0}>
+      <Grid container spacing={mdUp ? 1 : 0}>
         <Grid item xs={12} md={4}>
           <Img fluid={data.dog1.childImageSharp.fluid} />
         </Grid>
@@ -81,7 +82,7 @@ const Dog = () => {
                 </Grid>
                 <Grid item>
                   <Box my={2}>
-                    <Typography variant="body1">
+                    <Typography variant="body1" align="justify">
                       Lorem a dolor possimus minus magnam? Magnam modi at alias
                       exercitationem temporibus? Accusamus laudantium nihil
                       dolores blanditiis numquam Optio corporis neque tenetur
