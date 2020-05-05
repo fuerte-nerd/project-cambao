2f24c59a50f0f8497135f22deb09c408465e82e5
David Andrews <dandrewsuk82@gmail.com>
2020-05-05 14:11:58 +0100

    Autogit: 05-05-2020 14:11

--- src/pages/dog.js
+++ src/pages/dog.js
@@ -57,6 +57,27 @@ const Dog = () => {
                     <DogProfileRow label="Sterilized" info="Yes" />
                   </Grid>
                 </Grid>
+                <Divider />
+                <Grid item>
+                  <Box my={2}>
+                    <Typography>
+                      Lorem a dolor possimus minus magnam? Magnam modi at alias
+                      exercitationem temporibus? Accusamus laudantium nihil
+                      dolores blanditiis numquam Optio corporis neque tenetur
+                      quam animi a Assumenda atque quisquam asperiores quae
+                      deleniti. Repellat dolor voluptate iure tempore laborum Ex
+                      quasi consectetur cupiditate illum nihil eius Voluptate
+                      laborum ipsum minus dolorem dolore.\n\n Adipisicing ad
+                      nobis debitis eius velit Cum expedita nostrum quidem
+                      delectus quo id Id tempora beatae id soluta error? Odio in
+                      et expedita aperiam quas velit Vitae facere reiciendis
+                      accusamus consequatur eos distinctio! Officia sunt debitis
+                      voluptate accusantium recusandae Quisquam ratione
+                      accusamus placeat animi ex fuga? Veritatis velit earum
+                      nulla!
+                    </Typography>
+                  </Box>
+                </Grid>
               </Box>
             </Grid>
           </Box>
