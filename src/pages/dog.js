5852b71e0021d5c60aadb5faca52e896d0f1f559
David Andrews <dandrewsuk82@gmail.com>
2020-05-05 12:05:32 +0100

    Autogit: 05-05-2020 12:05

---
+++ src/pages/dog.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Box, Typography } from "@material-ui/core"
+
+const Dog = () => {
+  return (
+    <Box>
+      <Typography>Hello</Typography>
+    </Box>
+  )
+}
+
+export default Dog
