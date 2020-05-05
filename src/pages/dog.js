8cc2ea9d1f30b96dfee8d77351d2b4cbcb8cf36d
David Andrews <dandrewsuk82@gmail.com>
2020-05-05 12:15:43 +0100

    Autogit: 05-05-2020 12:15

--- src/pages/dog.js
+++ src/pages/dog.js
@@ -1,10 +1,33 @@
 import React from "react"
-import { Box, Typography } from "@material-ui/core"
+import { graphql, useStaticQuery } from "gatsby"
+import { Box, Typography, Grid } from "@material-ui/core"
+import Img from "gatsby-image"
 
 const Dog = () => {
+  const data = useStaticQuery(graphql`
+    {
+      dog1: file(name: { eq: "test" }) {
+        childImageSharp {
+          fluid(maxWidth: 400, maxHeight: 500, quality: 15) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
   return (
     <Box>
-      <Typography variant="h2">Buddy</Typography>
+      <Grid container>
+        <Grid item xs={12}>
+          <Typography variant="h2">Buddy</Typography>
+        </Grid>
+        <Grid item>
+          <Img fluid={data.dog1.childImageSharp.fluid} />
+        </Grid>
+        <Grid item>
+          <Grid container></Grid>
+        </Grid>
+      </Grid>
     </Box>
   )
 }
