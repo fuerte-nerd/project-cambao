06a89b5bd2a698b92d9d0db9e107b63320177c02
David Andrews <dandrewsuk82@gmail.com>
2020-05-06 11:19:40 +0100

    Autogit: 06-05-2020 11:19

--- src/pages/dog.js
+++ src/pages/dog.js
@@ -156,27 +156,14 @@ const Dog = () => {
                     <Typography>Video</Typography>
                   </ExpansionPanelSummary>
                   <ExpansionPanelDetails>
-                    <Box
-                      pb="56.25%"
-                      height={0}
-                      style={{ position: "relative" }}
-                    >
-                      <iframe
-                        width="560"
-                        height="315"
-                        src="https://www.youtube.com/embed/DgAw6jFo6Mw"
-                        frameborder="0"
-                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                        allowfullscreen
-                        style={{
-                          position: "absolute",
-                          top: 0,
-                          left: 0,
-                          width: "100%",
-                          height: "100%",
-                        }}
-                      ></iframe>
-                    </Box>
+                    <iframe
+                      width="100%"
+                      height="315"
+                      src="https://www.youtube.com/embed/DgAw6jFo6Mw"
+                      frameborder="0"
+                      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                      allowfullscreen
+                    ></iframe>
                   </ExpansionPanelDetails>
                 </ExpansionPanel>
               </Grid>
