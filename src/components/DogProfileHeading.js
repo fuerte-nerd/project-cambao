b274f2d916e342e6da0a5f839f325cbcca97b49b
David Andrews <dandrewsuk82@gmail.com>
2020-05-07 11:58:59 +0100

    Autogit: 07-05-2020 11:58

---
+++ src/components/DogProfileHeading.js
@@ -0,0 +1,26 @@
+import React from "react"
+import { Box, Typography, IconButton } from "@material-ui/core"
+import { MoreVert } from "@material-ui/icons"
+
+const DogProfileHeading = props => {
+  return props.mobile ? (
+    <Box
+      display="flex"
+      justifyContent="space-between"
+      alignItems="center"
+      bgcolor="white"
+      p={1}
+    >
+      <Box color="primary.dark">
+        <Typography variant="h2">Timanfaya</Typography>
+      </Box>
+      <Box>
+        <IconButton edge="end">
+          <MoreVert />
+        </IconButton>
+      </Box>
+    </Box>
+  ) : null
+}
+
+export default DogProfileHeading
