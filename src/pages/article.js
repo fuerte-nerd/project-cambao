a1bb6fcbf4edbb939b6e3115a98cb786940355be
David Andrews <dandrewsuk82@gmail.com>
2020-05-07 12:49:30 +0100

    Autogit: 07-05-2020 12:49

---
+++ src/pages/article.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Box, Typography } from "@material-ui/core"
+
+const Article = () => {
+  return (
+    <Box>
+      <Typography>Hello</Typography>
+    </Box>
+  )
+}
+
+export default Article
