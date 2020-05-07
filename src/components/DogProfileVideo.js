10d472a105e503d11eb67cdd0535dfe6213d09ba
David Andrews <dandrewsuk82@gmail.com>
2020-05-07 10:35:53 +0100

    Autogit: 07-05-2020 10:35

---
+++ src/components/DogProfileVideo.js
@@ -0,0 +1,32 @@
+import React from "react"
+import DogProfileExpansionPanel from "./DogProfileExpansionPanel"
+
+const DogProfileVideo = props => {
+  return (
+    <DogProfileExpansionPanel
+      title="Video"
+      headingVariant="h6"
+      expanded
+      youtube
+    >
+      <iframe
+        title="Video of dog"
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/DgAw6jFo6Mw"
+        frameborder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+        }}
+      ></iframe>
+    </DogProfileExpansionPanel>
+  )
+}
+
+export default DogProfileVideo
