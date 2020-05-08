be1c77da888172de4266ab93e725a0da7bb49c44
David Andrews <dandrewsuk82@gmail.com>
2020-05-08 13:26:11 +0100

    Autogit: 08-05-2020 13:26

--- src/components/index/Heading.js
+++ src/components/index/Heading.js
@@ -1,6 +1,12 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import { useTheme, useMediaQuery, Box, Typography } from "@material-ui/core"
+import {
+  useTheme,
+  useMediaQuery,
+  Container,
+  Box,
+  Typography,
+} from "@material-ui/core"
 import Img from "gatsby-image"
 
 const Heading = () => {
@@ -33,17 +39,18 @@ const Heading = () => {
           maxWidth: 150,
         }}
       />
-      <Box
-        ml={isNotMobile ? 2 : 0}
-        mt={isNotMobile ? 0 : 1}
-        align="center"
-        color="white"
-      >
-        <Typography variant="h3">We are their voice!</Typography>
-        <Typography variant="subtitle1">
-          Taking care of the abandoned and mistreated dogs of the La Oliva area
-          in Fuerteventura since 2013
-        </Typography>
+      <Box ml={isNotMobile ? 2 : 0} mt={isNotMobile ? 0 : 1} align="center">
+        <Box color="white">
+          <Typography variant="h3">We are their voice!</Typography>
+        </Box>
+        <Container>
+          <Box>
+            <Typography variant="subtitle1">
+              Taking care of the abandoned and mistreated dogs of the La Oliva
+              area in Fuerteventura since 2013
+            </Typography>
+          </Box>
+        </Container>
       </Box>
     </Box>
   )
