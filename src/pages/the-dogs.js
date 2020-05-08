167d817cae0bb4c480e3ec9b6a0da5b507ab742f
David Andrews <dandrewsuk82@gmail.com>
2020-05-08 13:21:06 +0100

    Autogit: 08-05-2020 13:21

--- src/pages/the-dogs.js
+++ src/pages/the-dogs.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Grid, Box, Typography } from "@material-ui/core"
+import { Container, Grid, Box, Typography } from "@material-ui/core"
 import SEO from "../components/seo"
 
 import DogListing from "../components/the-dogs/DogListing"
@@ -8,32 +8,36 @@ const TheDogs = () => {
   return (
     <>
       <SEO title="The Dogs" />
-      <Box mb={2} color="white">
+      <Box color="white">
         <Typography variant="h2">The Dogs</Typography>
-        <Typography variant="subtitle2">
-          We have lots of dogs waiting for their forever home. Come and meet
-          them! They can't wait to meet you!
-        </Typography>
       </Box>
-      <Box>
-        <Grid container spacing={1}>
-          <Grid item xs={12} sm={6} md={6} lg={4} xl={3}>
-            <DogListing />
+      <Container>
+        <Box mb={2}>
+          <Typography variant="subtitle2">
+            We have lots of dogs waiting for their forever home. Come and meet
+            them! They can't wait to meet you!
+          </Typography>
+        </Box>
+        <Box>
+          <Grid container spacing={1}>
+            <Grid item xs={12} sm={6} md={6} lg={4} xl={3}>
+              <DogListing />
+            </Grid>
+            <Grid item xs={12} sm={6} md={6} lg={4} xl={3}>
+              <DogListing />
+            </Grid>
+            <Grid item xs={12} sm={6} md={6} lg={4} xl={3}>
+              <DogListing />
+            </Grid>
+            <Grid item xs={12} sm={6} md={6} lg={4} xl={3}>
+              <DogListing />
+            </Grid>
+            <Grid item xs={12} sm={6} md={6} lg={4} xl={3}>
+              <DogListing />
+            </Grid>
           </Grid>
-          <Grid item xs={12} sm={6} md={6} lg={4} xl={3}>
-            <DogListing />
-          </Grid>
-          <Grid item xs={12} sm={6} md={6} lg={4} xl={3}>
-            <DogListing />
-          </Grid>
-          <Grid item xs={12} sm={6} md={6} lg={4} xl={3}>
-            <DogListing />
-          </Grid>
-          <Grid item xs={12} sm={6} md={6} lg={4} xl={3}>
-            <DogListing />
-          </Grid>
-        </Grid>
-      </Box>
+        </Box>
+      </Container>
     </>
   )
 }
