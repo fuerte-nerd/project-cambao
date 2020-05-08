30a1cb837a808864b97e8e94a7f1247e7caa7819
David Andrews <dandrewsuk82@gmail.com>
2020-05-08 13:05:51 +0100

    Autogit: 08-05-2020 13:05

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,13 @@
 import React from "react"
-import { Grid, Button, TextField, Box, Typography } from "@material-ui/core"
+import {
+  Grid,
+  Button,
+  TextField,
+  Box,
+  Typography,
+  Paper,
+  Container,
+} from "@material-ui/core"
 import { Send, Email } from "@material-ui/icons"
 import { FacebookMessenger } from "mdi-material-ui"
 import SEO from "../components/seo"
@@ -8,24 +16,29 @@ const Contact = () => {
   return (
     <>
       <SEO title="Contact" />
-      <Box color="white">
-        <Typography variant="h2">Contact us</Typography>
-      </Box>
-      <Box>
-        <Typography paragraph>
-          The most efficient method to get in touch with us is via Messenger...
-        </Typography>
-      </Box>
-      <Box>
-        <Button
-          size="large"
-          variant="contained"
-          color="secondary"
-          startIcon={<FacebookMessenger />}
-        >
-          Contact us on Messenger
-        </Button>
-      </Box>
+      <Paper>
+        <Container>
+          <Box color="white">
+            <Typography variant="h2">Contact us</Typography>
+          </Box>
+          <Box>
+            <Typography paragraph>
+              The most efficient method to get in touch with us is via
+              Messenger...
+            </Typography>
+          </Box>
+          <Box>
+            <Button
+              size="large"
+              variant="contained"
+              color="secondary"
+              startIcon={<FacebookMessenger />}
+            >
+              Contact us on Messenger
+            </Button>
+          </Box>
+        </Container>
+      </Paper>
       <Box mt={3} color="white">
         <Typography variant="h3">"I don't use Messenger..."</Typography>
       </Box>
