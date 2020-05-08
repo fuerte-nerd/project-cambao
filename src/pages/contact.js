9b192acab8c1c972f801f974b0f5c6db744ea3b9
David Andrews <dandrewsuk82@gmail.com>
2020-05-08 12:10:25 +0100

    Autogit: 08-05-2020 12:10

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,6 @@
 import React from "react"
-import { Grid, TextField, Box, Typography } from "@material-ui/core"
+import { Grid, Button, TextField, Box, Typography } from "@material-ui/core"
+import { FacebookMessenger } from "mdi-material-ui"
 import SEO from "../components/seo"
 
 const Contact = () => {
@@ -8,6 +9,24 @@ const Contact = () => {
       <SEO title="Contact" />
       <Box color="white">
         <Typography variant="h2">Contact us</Typography>
+      </Box>
+      <Box>
+        <Typography paragraph>
+          The most efficient method to get in touch with us is via Messenger...
+        </Typography>
+      </Box>
+      <Box>
+        <Button
+          size="large"
+          variant="contained"
+          color="secondary"
+          startIcon={<FacebookMessenger />}
+        >
+          Contact us on Messenger
+        </Button>
+      </Box>
+      <Box color="white">
+        <Typography variant="h3">"I don't use Messenger..."</Typography>
       </Box>
       <Grid container>
         <Grid item></Grid>
