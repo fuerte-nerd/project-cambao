9d8ea474060a4fa74da80a7f5bbe0102a9111930
David Andrews <dandrewsuk82@gmail.com>
2020-05-09 13:34:42 +0100

    Autogit: 09-05-2020 13:34

--- src/pages/article.js
+++ src/pages/article.js
@@ -54,133 +54,84 @@ const Article = () => {
               </Grid>
               <Grid item xs={12} md={6} lg={5}>
                 <CardContent>
-                  <Typography></Typography>
-                  <Typography></Typography>
-                  <Typography></Typography>
+                  <Typography variant="h2">
+                    This is the article heading
+                  </Typography>
+                  <Typography variant="overline">
+                    Posted on 12 April 2020 by FDR
+                  </Typography>
                 </CardContent>
               </Grid>
             </Grid>
           </CardActionArea>
-          <CardActions>
-            <IconButton>
+          <CardActions
+            style={{ background: theme.palette.secondary.main, color: "white" }}
+          >
+            <IconButton color="inherit">
               <Facebook />
             </IconButton>
-            <IconButton>
+            <IconButton color="inherit">
               <Twitter />
             </IconButton>
-            <IconButton>
+            <IconButton color="inherit">
               <WhatsApp />
             </IconButton>
-            <IconButton>
+            <IconButton color="inherit">
               <Email />
             </IconButton>
           </CardActions>
         </Card>
 
-        <Grid container spacing={2} alignItems="flex-end">
-          <Grid item xs={12}>
-            <Box bgcolor="#fafafa" boxShadow={2}>
-              <Grid container>
-                <Grid
-                  item
-                  xs={12}
-                  md={5}
-                  style={{ paddingTop: 0, paddingBottom: 0 }}
-                >
-                  <Img fluid={data.dog1.childImageSharp.fluid} />
-                </Grid>
-                <Grid item xs={12} md={7}>
-                  <Box
-                    pt={3}
-                    px={3}
-                    height="100%"
-                    display="flex"
-                    flexDirection="column"
-                    justifyContent="space-between"
-                  >
-                    <Box mb={xsDown ? 3 : 0} color="primary.dark">
-                      <Typography variant="h2">
-                        This is the article heading
-                      </Typography>
-                    </Box>
-                    <Box align="right">
-                      <Typography variant="overline">12 April 2020</Typography>
-                      <Typography variant="caption" display="block">
-                        Posted by FDR
-                      </Typography>
-                      <IconButton color="secondary">
-                        <Facebook />
-                      </IconButton>
-                      <IconButton color="secondary">
-                        <Twitter />
-                      </IconButton>
-                      <IconButton color="secondary">
-                        <WhatsApp />
-                      </IconButton>
-                      <IconButton edge="end" color="secondary">
-                        <Email />
-                      </IconButton>
-                    </Box>
-                  </Box>
-                </Grid>
-              </Grid>
-            </Box>
-          </Grid>
-          <Grid item xs={12}>
-            <Box color="#fafafa">
-              <Typography paragraph>
-                Consectetur aperiam fuga quod quo ea? Ab unde quos fuga corrupti
-                qui recusandae vero soluta recusandae. Ipsum minima quibusdam
-                placeat repellendus eaque Aliquam ullam velit sint vel ipsam
-                Aperiam neque laborum ad consectetur eius Iste nam eius nesciunt
-                ut consectetur! Quibusdam iusto laboriosam eos nulla eos. Unde
-                vero aspernatur beatae?
-              </Typography>
-              <Box pt={2} pb={4} maxWidth={400} margin="auto">
-                <Img fluid={data.dog2.childImageSharp.fluid} />
-              </Box>
-              <Typography paragraph>
-                Lorem ut unde laborum similique nisi laboriosam facilis nisi
-                provident Totam amet molestias veniam laudantium modi Culpa
-                eligendi ea dicta illum iure Numquam unde blanditiis fugit
-                facere perferendis? Dolore possimus pariatur fugit laborum
-                aliquid totam. Eius possimus sint vero numquam repellendus
-                Officia voluptas magni labore architecto dolore in Aliquid
-                deleniti voluptatem blanditiis officia eaque maxime architecto
-                recusandae Beatae recusandae aliquid a molestias doloremque non?
-                Labore aliquam saepe nihil voluptas consequatur Maiores veniam
-                ipsa itaque voluptatem
-              </Typography>
-              <Typography paragraph>
-                Dolor inventore iure placeat nemo ipsa! Quaerat id neque
-                voluptates unde ipsum iste? Voluptate aliquid voluptatum itaque
-                fugiat nobis. Reiciendis quaerat ratione soluta reprehenderit
-                maiores? Iure sed maiores possimus praesentium similique Laborum
-                aliquam impedit aliquid quos omnis Qui eos corrupti nobis
-                eligendi natus Aperiam incidunt earum assumenda ab ad Modi iusto
-                neque dolore aliquam ducimus Repellendus iure impedit vero
-                expedita non Cum culpa reiciendis minus temporibus tempora sed
-                dignissimos mollitia Quae vero aliquam dolor perferendis
-                recusandae fugiat quibusdam. Labore vero fuga voluptates aliquam
-                dolores Soluta dolore odit ad voluptas voluptas Vero perferendis
-                vitae nesciunt laudantium distinctio temporibus! Ipsum ab quo
-                nulla obcaecati assumenda odit expedita quas? Nobis voluptatum
-                animi tenetur delectus nihil? Eligendi dicta fugit consequatur
-                animi animi ab. Magni soluta qui iste doloribus praesentium?
-                Libero ab harum doloremque pariatur exercitationem Ut tenetur
-                laboriosam vel tenetur ratione harum Quod provident architecto
-                ducimus nam rem amet Veniam quo porro laborum quod blanditiis
-                Harum laudantium corporis illum necessitatibus et? Doloribus
-                tenetur cumque aspernatur provident hic voluptas Hic dolores
-                laborum numquam voluptatibus ut, ex Quibusdam tenetur nam fugiat
-                dignissimos maxime! Ratione provident nesciunt quisquam labore
-                expedita Harum explicabo inventore sint corrupti labore
-                Molestiae tempore consequuntur nemo labore ipsa fugiat. Nam non
-                ex eligendi.
-              </Typography>
-            </Box>
-          </Grid>
-        </Grid>
+        <Box color="#fafafa">
+          <Typography paragraph>
+            Consectetur aperiam fuga quod quo ea? Ab unde quos fuga corrupti qui
+            recusandae vero soluta recusandae. Ipsum minima quibusdam placeat
+            repellendus eaque Aliquam ullam velit sint vel ipsam Aperiam neque
+            laborum ad consectetur eius Iste nam eius nesciunt ut consectetur!
+            Quibusdam iusto laboriosam eos nulla eos. Unde vero aspernatur
+            beatae?
+          </Typography>
+          <Box pt={2} pb={4} maxWidth={400} margin="auto">
+            <Img fluid={data.dog2.childImageSharp.fluid} />
+          </Box>
+          <Typography paragraph>
+            Lorem ut unde laborum similique nisi laboriosam facilis nisi
+            provident Totam amet molestias veniam laudantium modi Culpa eligendi
+            ea dicta illum iure Numquam unde blanditiis fugit facere
+            perferendis? Dolore possimus pariatur fugit laborum aliquid totam.
+            Eius possimus sint vero numquam repellendus Officia voluptas magni
+            labore architecto dolore in Aliquid deleniti voluptatem blanditiis
+            officia eaque maxime architecto recusandae Beatae recusandae aliquid
+            a molestias doloremque non? Labore aliquam saepe nihil voluptas
+            consequatur Maiores veniam ipsa itaque voluptatem
+          </Typography>
+          <Typography paragraph>
+            Dolor inventore iure placeat nemo ipsa! Quaerat id neque voluptates
+            unde ipsum iste? Voluptate aliquid voluptatum itaque fugiat nobis.
+            Reiciendis quaerat ratione soluta reprehenderit maiores? Iure sed
+            maiores possimus praesentium similique Laborum aliquam impedit
+            aliquid quos omnis Qui eos corrupti nobis eligendi natus Aperiam
+            incidunt earum assumenda ab ad Modi iusto neque dolore aliquam
+            ducimus Repellendus iure impedit vero expedita non Cum culpa
+            reiciendis minus temporibus tempora sed dignissimos mollitia Quae
+            vero aliquam dolor perferendis recusandae fugiat quibusdam. Labore
+            vero fuga voluptates aliquam dolores Soluta dolore odit ad voluptas
+            voluptas Vero perferendis vitae nesciunt laudantium distinctio
+            temporibus! Ipsum ab quo nulla obcaecati assumenda odit expedita
+            quas? Nobis voluptatum animi tenetur delectus nihil? Eligendi dicta
+            fugit consequatur animi animi ab. Magni soluta qui iste doloribus
+            praesentium? Libero ab harum doloremque pariatur exercitationem Ut
+            tenetur laboriosam vel tenetur ratione harum Quod provident
+            architecto ducimus nam rem amet Veniam quo porro laborum quod
+            blanditiis Harum laudantium corporis illum necessitatibus et?
+            Doloribus tenetur cumque aspernatur provident hic voluptas Hic
+            dolores laborum numquam voluptatibus ut, ex Quibusdam tenetur nam
+            fugiat dignissimos maxime! Ratione provident nesciunt quisquam
+            labore expedita Harum explicabo inventore sint corrupti labore
+            Molestiae tempore consequuntur nemo labore ipsa fugiat. Nam non ex
+            eligendi.
+          </Typography>
+        </Box>
       </Container>
     </Box>
   )
