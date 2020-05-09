eaa9a50218f6332cb0955bc94556492d5127ea31
David Andrews <dandrewsuk82@gmail.com>
2020-05-09 16:29:15 +0100

    Autogit: 09-05-2020 16:29

--- src/pages/static.js
+++ src/pages/static.js
@@ -1,6 +1,7 @@
 import React from "react"
-import { Box } from "@material-ui/core"
+import { Box, Typography } from "@material-ui/core"
 import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 
 const Static = () => {
   const data = useStaticQuery(graphql`
@@ -16,7 +17,27 @@ const Static = () => {
   `)
   return (
     <Box>
-      <Box>Hello</Box>
+      <Box>
+        <Typography variant="h2">Page Title</Typography>
+      </Box>
+      <Grid container>
+        <Grid item>
+          <Img fluid={data.dog1.childImageSharp.fluid} />
+        </Grid>
+        <Grid item>
+          Consectetur ducimus laudantium assumenda doloremque est! Laboriosam
+          iste voluptas earum distinctio sint? Repellendus eligendi non repellat
+          praesentium dignissimos? At repellendus impedit aliquam aspernatur
+          molestiae Quis sapiente asperiores quibusdam similique officiis Aut
+          rem iure placeat pariatur itaque officia veritatis? Harum provident
+          accusantium voluptas praesentium velit. Quidem explicabo rerum totam
+          asperiores dolores. Inventore quidem impedit aliquid consequuntur ad.
+          Odio quisquam placeat vitae iure placeat qui? Hic aliquid?
+        </Grid>
+      </Grid>
+      <Box>
+        <Typography></Typography>
+      </Box>
     </Box>
   )
 }
