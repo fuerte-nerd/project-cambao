bb3a1fcbe38634ed51ca2f9acaeaed595f10c05c
David Andrews <dandrewsuk82@gmail.com>
2020-05-09 12:33:35 +0100

    Autogit: 09-05-2020 12:33

--- src/components/index/ArticleCard.js
+++ src/components/index/ArticleCard.js
@@ -31,36 +31,28 @@ const ArticleCard = () => {
         <CardActionArea>
           <Box display="flex">
             <CardMedia
-              style={{ height: "100%", width: "100%" }}
+              style={{ width: "100%" }}
               image={data.dog1.childImageSharp.fluid.src}
             />
             <CardContent>
-              <Grid container spacing={2}>
-                <Grid item xs={12} sm={5} md={4} lg={3}>
-                  <Img fluid={data.dog1.childImageSharp.fluid} />
-                </Grid>
-                <Grid item xs={12} sm={7} md={8} lg={9}>
-                  <Typography variant="h3">Post title</Typography>
-                  <Typography variant="overline">27 April 2020</Typography>
-                  <Box mb={2}>
-                    <Divider />
-                  </Box>
+              <Typography variant="h3">Post title</Typography>
+              <Typography variant="overline">27 April 2020</Typography>
+              <Box mb={2}>
+                <Divider />
+              </Box>
 
-                  <Typography align="justify" paragraph>
-                    Lorem possimus non perspiciatis quibusdam iste. Numquam
-                    veritatis consequatur velit ea ad quia? Deleniti autem
-                    dolorum consequatur labore natus. Obcaecati assumenda
-                    debitis quas accusantium temporibus animi! Facilis molestias
-                    ab necessitatibus similique itaque Exercitationem assumenda
-                    nemo.
-                  </Typography>
-                  <Box align="right" width="100%">
-                    <Button variant="contained" color="secondary">
-                      Read more
-                    </Button>
-                  </Box>
-                </Grid>
-              </Grid>
+              <Typography align="justify" paragraph>
+                Lorem possimus non perspiciatis quibusdam iste. Numquam
+                veritatis consequatur velit ea ad quia? Deleniti autem dolorum
+                consequatur labore natus. Obcaecati assumenda debitis quas
+                accusantium temporibus animi! Facilis molestias ab
+                necessitatibus similique itaque Exercitationem assumenda nemo.
+              </Typography>
+              <Box align="right" width="100%">
+                <Button variant="contained" color="secondary">
+                  Read more
+                </Button>
+              </Box>
             </CardContent>
           </Box>
         </CardActionArea>
