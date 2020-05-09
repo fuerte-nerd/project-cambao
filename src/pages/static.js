af2d0434c63e0c6dae7cf199bbd55d43ce8f186c
David Andrews <dandrewsuk82@gmail.com>
2020-05-09 16:34:22 +0100

    Autogit: 09-05-2020 16:34

--- src/pages/static.js
+++ src/pages/static.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Box, Typography } from "@material-ui/core"
+import { Box, Grid, Typography } from "@material-ui/core"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 
